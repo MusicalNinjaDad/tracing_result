4d6ccd6b76e5ffe4e05059695cdc4eeb2d2a4263
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-31 15:04:39 +0000

    fix pretty example

--- examples/pretty.rs
+++ examples/pretty.rs
@@ -10,25 +10,9 @@ fn main() -> io::Result<()> {
 
     let answer = 42;
 
-    // Demonstrate or_* methods - log on Err
-    warn_span!("or_warn example", answer);
-    Err(io::Error::other("Oopsie")).or_warn("This will log a WARN on error")?;
-
-    // Demonstrate and_* methods - log on Ok
     info_span!("and_warn example");
     Ok(42).and_warn("This will log a WARN on success")?;
 
-    // Demonstrate all log levels with or_* (error case)
-    error_span!("or_error example");
-    Err(io::Error::other("Critical")).or_error("This will log an ERROR on error")?;
-
-    debug_span!("or_debug example");
-    Err(io::Error::other("Debug issue")).or_debug("This will log a DEBUG on error")?;
-
-    trace_span!("or_trace example");
-    Err(io::Error::other("Trace issue")).or_trace("This will log a TRACE on error")?;
-
-    // Demonstrate all log levels with and_* (success case)
     error_span!("and_error example");
     Ok(42).and_error("This will log an ERROR on success")?;
 
@@ -37,6 +21,18 @@ fn main() -> io::Result<()> {
 
     trace_span!("and_trace example");
     Ok(42).and_trace("This will log a TRACE on success")?;
+
+    warn_span!("or_warn example", answer);
+    Err(io::Error::other("Oopsie")).or_warn("This will log a WARN on error")?;
+
+    error_span!("or_error example");
+    Err(io::Error::other("Critical")).or_error("This will log an ERROR on error")?;
+
+    debug_span!("or_debug example");
+    Err(io::Error::other("Debug issue")).or_debug("This will log a DEBUG on error")?;
+
+    trace_span!("or_trace example");
+    Err(io::Error::other("Trace issue")).or_trace("This will log a TRACE on error")?;
 
     Ok(())
 }
