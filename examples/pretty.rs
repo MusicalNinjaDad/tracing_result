2464ecee7cf0b29eef2db3096fc5bac7381556ea
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-31 17:55:24 +0000

    fix spans in example

--- examples/pretty.rs
+++ examples/pretty.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use tracing::{debug_span, error_span, info_span, trace_span, warn_span};
+use tracing::error_span;
 use tracing_result::*;
 use tracing_subscriber::{filter::LevelFilter, fmt::layer, prelude::*, registry};
 
@@ -8,30 +8,21 @@ fn main() -> io::Result<()> {
     let stdout = layer().pretty().with_filter(LevelFilter::TRACE);
     registry().with(stdout).init();
 
-    let answer = 42;
-
-    info_span!("and_warn example");
+    let _span = error_span!("pretty examples").entered();
     Ok(42).and_warn("This will log a WARN on success")?;
 
-    error_span!("and_error example");
     Ok(42).and_error("This will log an ERROR on success")?;
 
-    debug_span!("and_debug example");
     Ok(42).and_debug("This will log a DEBUG on success")?;
 
-    trace_span!("and_trace example");
     Ok(42).and_trace("This will log a TRACE on success")?;
 
-    warn_span!("or_warn example", answer);
     Err(io::Error::other("Oopsie")).or_warn("This will log a WARN on error")?;
 
-    error_span!("or_error example");
     Err(io::Error::other("Critical")).or_error("This will log an ERROR on error")?;
 
-    debug_span!("or_debug example");
     Err(io::Error::other("Debug issue")).or_debug("This will log a DEBUG on error")?;
 
-    trace_span!("or_trace example");
     Err(io::Error::other("Trace issue")).or_trace("This will log a TRACE on error")?;
 
     Ok(())
