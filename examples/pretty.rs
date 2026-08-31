ad3d741239d365352f4a1a55cb8b9fdc82deb257
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-31 09:07:52 +0000

    not picking up span ... abort for now & pick this up again later

--- examples/pretty.rs
+++ examples/pretty.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+use tracing::warn_span;
 use tracing_result::*;
 use tracing_subscriber::{filter::LevelFilter, fmt::layer, prelude::*, registry};
 
@@ -7,6 +8,9 @@ fn main() -> io::Result<()> {
     let stdout = layer().pretty().with_filter(LevelFilter::TRACE);
     registry().with(stdout).init();
 
+    let answer = 42;
+
+    warn_span!("pretty example", answer);
     Err(io::Error::other("Oopsie")).and_warn("boom!")?;
 
     Ok(())
